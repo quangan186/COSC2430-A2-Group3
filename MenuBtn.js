647c7148564c0700de7da614669fbde2743a51ad
Bui Quang An <quangan186@gmail.com>
2022-04-18 10:38:50 +0700

    header + footer

---
+++ MenuBtn.js
@@ -0,0 +1,9 @@
+const menuButton = () => {
+    document.getElementById("menu_2").style.transform = "translateX(0)";
+    document.getElementById("menu_2").style.opacity = "1";
+}
+
+const closeButton = () => {
+    document.getElementById("menu_2").style.transform = "translateX(100%)";
+    document.getElementById("menu_2").style.opacity = "0";
+}

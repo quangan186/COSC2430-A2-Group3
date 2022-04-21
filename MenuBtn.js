0337441885580080905714edb2aa90c4456660d9
Bui Quang An <quangan186@gmail.com>
2022-04-22 00:20:04 +0700

    add anim for about + about content (1)

--- MenuBtn.js
+++ MenuBtn.js
@@ -1,9 +1,25 @@
 const menuButton = () => {
-    document.getElementById("phone_menu").style.transform = "translateX(0)";
-    document.getElementById("phone_menu").style.opacity = "1";
+    document.getElementById("phone-menu").style.transform = "translateX(0)";
+    document.getElementById("phone-menu").style.opacity = "1";
 }
 
 const closeButton = () => {
-    document.getElementById("phone_menu").style.transform = "translateX(100%)";
-    document.getElementById("phone_menu").style.opacity = "0";
+    document.getElementById("phone-menu").style.transform = "translateX(100%)";
+    document.getElementById("phone-menu").style.opacity = "0";
 }
+
+window.addEventListener('scroll', () => {
+    const scrolled = window.scrollY;
+    const developers = document.querySelectorAll(".developer");
+    if (scrolled > 0){ 
+        developers.forEach(dev => {
+            dev.style.transform = "translateX(0)";
+            dev.style.opacity = "1";
+        })
+    } else{
+        developers.forEach(dev => {
+            dev.style.transform = "translateX(-200%)";
+            dev.style.opacity = "0";
+        })
+    }
+})

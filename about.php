d0d606f1afba60b490ce9976685cd5509df1ee82
Bui Quang An <quangan186@gmail.com>
2022-04-22 21:04:00 +0700

    change header, footer

---
+++ about.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>InstaKilogram</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="./css/header.css">
+    <link rel="stylesheet" href="./css/footer.css">
+    <link rel="stylesheet" href="./css/about.css">
+</head>
+<body>
+    <?php include("./support/header.php") ?>
+
+    <main>
+        <div class="about-instakilogram">
+            <h1>InstaKilogram</h1>
+            <p>InstaKilogram is an social media website developed by a group of students in RMIT University. Based on Instagram, one of the most popular applications which users use to share a number of beautiful images. In addition to sharing personal photos online to get people's attention and interaction, this is one of ways to save their images instead of saving them on phones or computers, which consumes a lot of device's storage.</p>
+        </div>
+
+        <div class="developers-container">
+            <h2>About us</h3>
+            <div class="developer">
+                <div class="avatar">
+                    <img src="https://w0.peakpx.com/wallpaper/666/961/HD-wallpaper-anime-jujutsu-kaisen-satoru-gojo.jpg" alt="An's ava">
+                </div>
+
+                <div class="info">
+                    <ul>
+                        <h1>Bui Quang An</h1>
+                        <li>Graduated from Nguyen Thi Minh Khai High school in HCMC</li>
+                        <li>IT Sophomore at RMIT University</li>
+                        <li>Hobbies: Listening to music, playing sports, playing guitar</li>
+                        <p>"Honestly, I'm a funny, talkative and responsible person. In the future, I would like to become a web and game developer"</p>
+                    </ul>
+                </div>
+            </div>
+
+            <div class="developer">
+                <div class="avatar">
+                    <img src="https://w0.peakpx.com/wallpaper/666/961/HD-wallpaper-anime-jujutsu-kaisen-satoru-gojo.jpg" alt="An's ava">
+                </div>
+
+                <div class="info">
+                    <ul>
+                        <h1>Bui Quang An</h1>
+                        <li>Graduated from Nguyen Thi Minh Khai High school in HCMC</li>
+                        <li>IT Sophomore at RMIT University</li>
+                        <li>Hobbies: Listening to music, playing sports, playing guitar</li>
+                        <p>"Honestly, I'm a funny, talkative and responsible person. In the future, I would like to become a web and game developer"</p>
+                    </ul>
+                </div>
+            </div>
+
+            <div class="developer">
+                <div class="avatar">
+                    <img src="https://w0.peakpx.com/wallpaper/666/961/HD-wallpaper-anime-jujutsu-kaisen-satoru-gojo.jpg" alt="An's ava">
+                </div>
+
+                <div class="info">
+                    <ul>
+                        <h1>Bui Quang An</h1>
+                        <li>Graduated from Nguyen Thi Minh Khai High school in HCMC</li>
+                        <li>IT Sophomore at RMIT University</li>
+                        <li>Hobbies: Listening to music, playing sports, playing guitar</li>
+                        <p>"Honestly, I'm a funny, talkative and responsible person. In the future, I would like to become a web and game developer"</p>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        
+    </main>
+
+    <?php include("./support/footer.php") ?>
+</body>
+<script src="./js/MenuBtn.js"></script>
+<script src="./js/ShowInfo.js"></script>
+</html>
---
+++ support/footer.php
@@ -0,0 +1,16 @@
+<footer>
+        <div class="copyright-privacy">
+            <nav>
+                <h3>All Right Reserved. © 2022 InstaKilogram.</h3>
+
+                <ul>
+                    <li><a href="./about/about.php">About</a></li>
+                    <li><a href="#">Copyright</a></li>
+                    <li><a href="#">Privacy</a></li>
+                    <li><a href="#">Help</a></li>
+                </ul>
+
+                <h3>Developed by group 3</h3>
+            </nav>
+        </div>
+</footer>

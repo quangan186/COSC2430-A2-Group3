fc3aa12294845f9b3409148666816fb48236ebfe
notrealchauloan <75651801+notrealchauloan@users.noreply.github.com>
2022-04-23 18:37:49 +0700

    Create Post UI

---
+++ createPosts.php
@@ -0,0 +1,16 @@
+<form method="POST" class="create-post" enctype="multipart/form-data">
+    <div class="profile-photo">
+        <img src="images/profile-1.jpg" alt="">
+    </div>
+    <input name="post" type="text" placeholder="What's on your mind?" id="create-post">
+    <label for="file-upload" class="custom-file-upload">
+        <input id="file-upload" type="file" name="file">
+        <!-- <i class="uil uil-image-upload" style="font-size: 30px";> </i> -->
+    </label>
+    <div class="create">
+        <label for="submit_post">
+            <input class="btn btn-primary" type="submit" value="Post" type="text">
+        </label>
+    </div>
+</form>
+
